bc698797160dd12ea2521896073e55f7c98368de
Valera <v.mischenko90@gmail.com>
2025-09-20 17:00:08 +0300

    change host to match koyeb requirements

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ The module configures:
 - Development server configuration
 """
 
+import os
 from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -62,5 +63,5 @@ app.include_router(users.router, prefix="/api")
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run("src.main:app", host="0.0.0.0", port=port, reload=False)
